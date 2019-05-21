478ee061787f613912a471badde04d98f525fc90
DoanQuangDuc <dqduc.1512121@gmail.com>
2019-05-21 23:58:42 +0700

    Add login form

--- Applications/SPA/Client-App/src/app/feature/login/login.component.ts
+++ Applications/SPA/Client-App/src/app/feature/login/login.component.ts
@@ -10,9 +10,9 @@ export class LoginComponent implements OnInit {
   constructor(
     private _apiHttp: AuthHttpService
   ) { }
-  model = {
-    username: '',
-    password: ''
+  user = {
+    userName: '',
+    passWord: ''
   };
 
   ngOnInit() {
